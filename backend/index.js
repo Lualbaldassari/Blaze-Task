a07a3499ff661cdde2287c305f95f1cee6d04154
lualbaldassari <lualbaldassari@gmail.com>
2022-11-19 12:16:34 -0500

    get ready

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,22 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "panzerfaust12345",
-  database: "test",
+  database: "blaze",
 });
 
 app.get("/", (req, res) => {
   res.json("hello this is the backend!");
 });
+
+app.get("/products", (req, res) => {
+  const q = "SELECT * FROM products";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+
 
 
 app.listen(8800, ()=>{
